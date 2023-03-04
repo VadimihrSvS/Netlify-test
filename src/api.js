d02c29e3bddebd1335f871d8e19db8d25b9c1d56
Vadim Sokolovsky <vadimirsvs@gmail.com>
2023-03-04 15:05:01 +0300

    asd

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const serverless = require("serverless-http");
 //const mongoose = require("mongoose");
-//const _ = require("lodash");
+const _ = require("lodash");
 
 const app = express();
 const router = express.Router();
