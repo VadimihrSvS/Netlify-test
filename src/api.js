8bb3efa04730f929ce0a0471cf3c824f499bd5fb
Vadim Sokolovsky <vadimirsvs@gmail.com>
2023-03-04 14:59:32 +0300

    asd

--- src/api.js
+++ src/api.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const app = express();
 const router = express.Router();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb+srv://admin-vadimirsvs:10012015aq@cluster0.y2f6gkt.mongodb.net/todolistDB");
 
 router.get("/", (req, res) => {
     res.json({
